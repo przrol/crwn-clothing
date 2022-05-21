00ee46c90baab2730de68d6803ec77f53489798d
roland <you@example.com>
2022-05-21 12:13:08 +0200

    signInWithGoogleRedirect added

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,29 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import { async } from "@firebase/util";
 
 const SignIn = () => {
+  useEffect(() => {
+    async function redirectResult() {
+      // You can await here
+      const response = await getRedirectResult(auth);
+      if(response) {
+          const userDocRef = await createUserDocumentFromAuth(response.user)
+      }
+      // ...
+    }
+    redirectResult();
+  }, []); // Or [] if effect doesn't need props or state
+
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
@@ -10,6 +31,9 @@ const SignIn = () => {
     <div>
       <h1>Sign in Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
